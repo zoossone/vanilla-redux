8f29ffdb9a208be61951c6be2ef2f409fe0f7bed
Chu Sangwon <zoossone@gmail.com>
2021-05-17 15:19:23 +0900

    improve

--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD"; // string의 오타를 쉽게 잡아줄 수 있다.
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {  
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1; // 리턴하는것이 곧 데이터이다.
+  switch (action.type) { // action에는 항상 type property가 존재해야 한다. switch를 쓰는편이 좋다
+    case "ADD":
+      return count + 1;
+    case "MINUS":
+      return count - 1;
+    default:
+      return count;
   }
-  return count;
 } // state는 이곳에서만 modify - 다른 어떤 함수에서도 할 수 없음
 
 const countStore = createStore(countModifier); // store는 데이터를 저장하는 곳이다. 크리에이트 스토어애는 반드시 함수가 들어간다
@@ -23,7 +28,7 @@ const onClick = () => {
 
 countStore.subscribe(onClick);
 
-plus.addEventListener("click", () => { countStore.dispatch({ type: "ADD" }) }); // 디스패치로 액션을 보내준다.
-minus.addEventListener("click", () => { countStore.dispatch({ type: "MINUS" }) });
+plus.addEventListener("click", () => { countStore.dispatch({ type: ADD }) }); // 디스패치로 액션을 보내준다.
+minus.addEventListener("click", () => { countStore.dispatch({ type: MINUS }) });
 
 console.log(countStore.getState()); // return된 data(state)를 보여줌
