f7783fc2df0fa0603e19c8b624d338e8ee807e85
Chu Sangwon <zoossone@gmail.com>
2021-05-17 14:54:41 +0900

    getState dipatch subscribe

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const plus = document.querySelector("#add");
 const minus = document.querySelector("#minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {  
   if (action.type === "ADD") {
     return count + 1;
   } else if (action.type === "MINUS") {
@@ -17,11 +17,13 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier); // store는 데이터를 저장하는 곳이다. 크리에이트 스토어애는 반드시 함수가 들어간다
 
-countStore.dispatch({ type: "ADD" }); // reducer(countModifier) 밖에서 메세지(action 객체)를 보내준다.
-countStore.dispatch({ type: "ADD" }); // data의 모든 관리는 reducer 안에서만 이루어지므로 밖에서는 객체를 보내줘서 관리해준다. 이것이 redux가 해주는 일.
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onClick = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onClick);
+
+plus.addEventListener("click", () => { countStore.dispatch({ type: "ADD" }) }); // 디스패치로 액션을 보내준다.
+minus.addEventListener("click", () => { countStore.dispatch({ type: "MINUS" }) });
 
 console.log(countStore.getState()); // return된 data(state)를 보여줌
