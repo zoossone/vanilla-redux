e720ba1692a39718a08bf80072495085166bcd06
Chu Sangwon <zoossone@gmail.com>
2021-05-17 14:30:18 +0900

    createSore1

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ const countModifier = (count = 0) => {
   return count;
 } // state는 이곳에서만 modify - 다른 어떤 함수에서도 할 수 없음
 
-const countStore = createStore(countModifier); // 크리에이트 스토어애는 반드시 함수가 들어간다
+const countStore = createStore(countModifier); // store는 데이터를 저장하는 곳이다. 크리에이트 스토어애는 반드시 함수가 들어간다
 
 console.log(countStore.getState()); // return된 data(state)를 보여줌
