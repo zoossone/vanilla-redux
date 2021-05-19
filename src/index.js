32291762a294f4f3de5a60e11650bb6af109c1b1
Chu Sangwon <zoossone@gmail.com>
2021-05-19 17:23:20 +0900

    did redux basic again :)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import { createStore } from "redux";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const number  = document.querySelector("span");
 
-const createTodo = () => {
-  const li = document.createElement("li");
-  li.innerText
+const ADD = "ADD"; // 오타로 인한 실수를 줄이기 위해서
+const MINUS = "MINUS";
+
+number.innerText = 0; // HTML을 보여주기 위한 초기화
+
+
+/**
+ * 보통 리듀서에서는 초기화를 사용한다. state값이 설정되어있지 않으면 initailValue를 넣어줄 수 있다.
+ * reducer에서 반환하는 어떤값이든 그게 곧 상태가 된다.
+ * 그리고 data의 modify는 오직 리듀서에서만 이뤄진다. 다른 함수에서는 절대 관리할 수 없다.
+ * action 객체를 보내줘서 reducer를 동작할 수 있다.
+ * 
+ */
+const reducer = (count = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
+
+const countStore = createStore(reducer);
+
+const onClick = () => {
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onClick); // statre의 변화를 구독해서 따라갈 수 있다.
+
+plus.addEventListener("click", () => { countStore.dispatch({ type: ADD }) });
+minus.addEventListener("click", () => { countStore.dispatch({ type: MINUS }) });
