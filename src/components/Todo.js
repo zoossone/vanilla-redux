521c8848958eaeba37d72905958a34eb3cbe0150
Chu Sangwon <zoossone@gmail.com>
2021-06-27 23:42:42 +0900

    really done :)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,15 @@
 import React from "react";
 import {connect} from "react-redux";
 import {actionCreators} from "../store";
+import {Link} from "react-router-dom";
 
 
-function Todo({text, onBtnClick}) {
+function Todo({text, onBtnClick, id}) {
     return <li>
-        {text}<button onClick={onBtnClick}>DEL</button>
+        <Link to={`/${id}`}>
+        {text}
+        </Link>
+        <button onClick={onBtnClick}>DEL</button>
         </li>
 }
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,24 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+
+function Detail({toDo}) {
+    return <>
+        <h1>{toDo?.text}</h1>
+        <h2>CreatedAt: {toDo?.id}</h2>
+    </>
+}
+
+const mapStateToProps = (state, ownProps) => {
+    const { match:
+        { params:
+            {id} } // 구조분해할당때 괄호에 안 넣으면 그냥 params가 대체된다.
+    } = ownProps;
+
+    console.log(ownProps)
+    console.log(state)
+    return {
+        toDo: state.find(todo => todo.id === parseInt(id))
+    }
+}
+
+export default connect(mapStateToProps)(Detail);
